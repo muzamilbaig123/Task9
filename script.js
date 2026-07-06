13a0b35dd18cbff526a4c16b992c08e03bb31811
muzamilbaig123 <muzamilbaig24@gmail.com>
2026-07-06 17:50:40 +0500

    html css ui is ready and work js start

---
+++ script.js
@@ -0,0 +1,55 @@
+// Navigation Dashboard
+const dashboardView = document.getElementById("dashboardView");
+const featureCards = document.querySelectorAll(".feature-card");
+const featureSections = document.querySelectorAll(".feature-section");
+const backButtons = document.querySelectorAll(".back-btn");
+
+// Todo List
+const todoForm = document.getElementById("todoForm");
+const todoInput = document.getElementById("todoInput");
+const todoList = document.getElementById("todoList");
+
+// Daily Planner
+const plannerList = document.getElementById("plannerList");
+const plannerInputs = document.querySelectorAll(".planner-input");
+
+// Motivation Quote
+const newQuoteBtn = document.getElementById("newQuoteBtn");
+const quoteText = document.getElementById("quoteText");
+const quoteAuthor = document.getElementById("quoteAuthor");
+const quoteLoader = document.getElementById("quoteLoader");
+
+// Pomodoro Timer
+const timerDisplay = document.getElementById("timerDisplay");
+const sessionLabel = document.getElementById("sessionLabel");
+const startBtn = document.getElementById("startBtn");
+const pauseBtn = document.getElementById("pauseBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+// Daily Goals
+const goalForm = document.getElementById("goalForm");
+const goalInput = document.getElementById("goalInput");
+const goalsList = document.getElementById("goalsList");
+const goalsProgress = document.getElementById("goalsProgress");
+
+// Weather Widget
+const weatherIcon = document.getElementById("weatherIcon");
+const weatherTemp = document.getElementById("weatherTemp");
+const weatherLocation = document.getElementById("weatherLocation");
+
+// Date and Time
+const currentTime = document.getElementById("currentTime");
+const currentDate = document.getElementById("currentDate");
+
+// Dynamic BAckgound
+const dynamicBg = document.getElementById("dynamicBg");
+
+// Theme Switcher
+const themeToggleBtn = document.getElementById("themeToggleBtn");
+const htmlRoot = document.documentElement;
+
+
+console.log(dashboardView)
+console.log(featureCards)
+console.log(featureSections)
+console.log(backButtons)
